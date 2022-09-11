f57aebf503355f6f3197c75516d1ae6d28ae9452
YOUPIDOK <58979097+YOUPIDOK@users.noreply.github.com>
2022-09-11 11:33:59 +0200

    Add RgpdTrait

--- src/Entity/GCU.php
+++ src/Entity/GCU.php
@@ -13,81 +13,10 @@ use Symfony\Component\Validator\Constraints\NotNull;
 class GCU
 {
     use TimestampableEntity;
-
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
-
-    #[ORM\Column(type: Types::STRING)]
-    #[NotNull]
-    private ?string $versionNumber = null;
-
-    #[ORM\Column(type: Types::TEXT)]
-    #[NotNull]
-    private ?string $body = null;
-
-    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
-    private ?\DateTime $implementationDate = null;
-
-    #[ORM\Column]
-    private ?bool $isDraft = true;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+    use RgpdTrait;
 
     public function __toString(): string
     {
         return "Conditions générales d'utilisation" . ($this->implementationDate !== null ? (' du ' . $this->implementationDate->format('d/m/Y h:i:s')   ) : '') ;
-    }
-
-    public function getVersionNumber(): ?string
-    {
-        return $this->versionNumber;
-    }
-
-    public function setVersionNumber(?string $versionNumber): self
-    {
-        $this->versionNumber = $versionNumber;
-
-        return $this;
-    }
-
-    public function getBody(): ?string
-    {
-        return $this->body;
-    }
-
-    public function setBody(?string $body): self
-    {
-        $this->body = $body;
-
-        return $this;
-    }
-
-    public function getImplementationDate(): ?\DateTime
-    {
-        return $this->implementationDate;
-    }
-
-    public function setImplementationDate(\DateTime $implementationDate): self
-    {
-        $this->implementationDate = $implementationDate;
-
-        return $this;
-    }
-
-    public function isDraft(): ?bool
-    {
-        return $this->isDraft;
-    }
-
-    public function setIsDraft(bool $isDraft): self
-    {
-        $this->isDraft = $isDraft;
-
-        return $this;
     }
 }
